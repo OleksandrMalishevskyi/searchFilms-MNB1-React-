83bbb0ffe6bc1b7df5f665b29478dd3f02d9c85c
Aleksandr <oleksandr.malishevskyi@gmail.com>
2022-02-13 15:53:31 +0100

    start project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Footer from "./layout/Footer";
 import Header from "./layout/Header";
+import Main from "./layout/Main";
 
 function App() {
   return (
     <>
       <Header />
+      <Main/>
+      <Footer />
     </>
   );
 }
